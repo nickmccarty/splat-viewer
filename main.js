731f6d659f87b064d7037b10399d84696feab2a7
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2024-11-25 13:37:03 -0800

    fixing CORS error

--- main.js
+++ main.js
@@ -751,7 +751,7 @@ async function main() {
     );
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
-        credentials: "*", // include, *same-origin, omit
+        credentials: "no-cors", // include, *same-origin, omit
     });
     console.log(req);
     if (req.status != 200)
