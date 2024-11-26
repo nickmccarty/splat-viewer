ca578479200d1436c64c3a1bd2f339207c993704
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2024-11-25 19:57:50 -0800

    resolved CORS issue

--- main.js
+++ main.js
@@ -750,7 +750,7 @@ async function main() {
         "https://huggingface.co/nickmccarty/splat-data/resolve/main",
     );
     const req = await fetch(url, {
-        mode: "cors", // no-cors, *cors, same-origin
+        mode: "no-cors", // no-cors, *cors, same-origin
         credentials: "omit", // include, *same-origin, omit
     });
     console.log(req);
