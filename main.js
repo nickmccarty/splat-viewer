b009d4e36f4022b5406d1e5d32f32ca1bdf7f821
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2024-11-26 16:02:12 -0800

    linked to improved splat

--- main.js
+++ main.js
@@ -746,7 +746,7 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "small-city.splat",
+        params.get("url") || "small-city-accel.splat",
         "https://huggingface.co/nickmccarty/splat-data/resolve/main/",
     );
     const req = await fetch(url, {
