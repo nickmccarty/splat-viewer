567712926e1ee205a0d64c8a58cd920e35c8ecb1
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2024-12-01 10:07:08 -0800

    added colosseum exterior as default splat

--- main.js
+++ main.js
@@ -746,7 +746,7 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "brandenburg_gate.splat",
+        params.get("url") || "colosseum_exterior.splat",
         "https://huggingface.co/nickmccarty/splat-data/resolve/main/",
     );
     const req = await fetch(url, {
