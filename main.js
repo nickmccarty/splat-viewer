0b15705f43f61dbb04479dcbd829b13a8dcc6da1
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2024-12-01 09:07:36 -0800

    added brandenburg gate splat as default

--- main.js
+++ main.js
@@ -746,7 +746,7 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "small-city-accel.splat",
+        params.get("url") || "brandenburg_gate.splat",
         "https://huggingface.co/nickmccarty/splat-data/resolve/main/",
     );
     const req = await fetch(url, {
