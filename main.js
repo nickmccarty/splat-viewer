08f0f8ab8917d81c880d17ad71fddac3f15ba21d
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2025-02-06 17:34:41 +0800

    made Taj Mahal the default splat

--- main.js
+++ main.js
@@ -746,7 +746,7 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "dalle-lego-dozer.splat",
+        params.get("url") || "taj_mahal.splat",
         "https://huggingface.co/nickmccarty/splat-data/resolve/main/",
     );
     const req = await fetch(url, {
