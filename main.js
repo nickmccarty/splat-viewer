c7ab55e2f8d2ccfa3fbc153bea7f904279b23503
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2025-02-06 23:43:36 +0800

    made monarch butterfly the default splat

--- main.js
+++ main.js
@@ -746,7 +746,7 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "taj_mahal.splat",
+        params.get("url") || "monarch-butterfly.splat",
         "https://huggingface.co/nickmccarty/splat-data/resolve/main/",
     );
     const req = await fetch(url, {
